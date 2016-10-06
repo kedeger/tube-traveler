82a1018864c83eac4f8a54c411bc4b052a81a854
kedeger <kedeger@uw.edu>
2016-10-05 18:55:35 -0700

    scraping youtube url from playbackfm, still need to send url to html as ng-src

--- index.js
+++ index.js
@@ -1,12 +1,90 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
+var app = express()
 var path = require('path');
+var request = require('request');
+var jsdom = require('jsdom');
+var url = require('url');
+var cheerio = require('cheerio');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/scrape/:year', function(req, res) {
+
+     request('http://playback.fm/year/' +req.params.year, function (error, response, data) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(data);
+    // var iframe = $('iframe');
+    // console.log(iframe);
+    // res.send({data: data});
+    var youtubeId = $('.youtubeID')[0].attribs.value;
+    var link = 'https://www.youtube.com/watch?v='+youtubeId;
+    res.send({link: link})
+
+    // res.send({link: link})
+    // .map(function(index, element) {
+    // return {
+    //   link: $(element).attr('src')
+    // };
+  // })
+
+    // .get();
+
+    // console.log(videoUrls);
+
+
+  //   var links = $('iframe:first').contents()[0].links;
+
+  // console.log(links);
+  }
+})
+})
 
 app.get('/*', function(req, res) {
  res.sendFile(__dirname + '/index.html');
 });
+
+
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// app.get('/scrape/:year', function(req, res) {
+//   url = 'http://playback.fm/year/'+req.params.year
+
+//    request(url, function(error, response, data){
+//         if(!error){
+//             var $ = cheerio.load(data);
+
+//             var videoUrls = $('iframe').map(function(index, element) {
+//     return {
+//       link: $(element).closest('a').attr('src')
+//     };
+//   }).get();
+
+//   console.log(videoUrls);
+
+//             // var videoUrl;
+//             // var json = { videoUrl : ""};
+
+//         }
+//     })
+
+// });
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// request('http://playback.fm/year/1910/', function (error, response, data) {
+//   var $ = cheerio.load(data);
+
+//   var videoUrls = $('iframe').map(function(index, element) {
+//     return {
+//       link: $(element).closest('a').attr('src')
+//     };
+//   }).get();
+
+//   console.log(videoUrls);
+// });
+
+
+
 
 app.listen(3000);
--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,14 @@
 var tubeTravelApp = angular.module('TubeTravelApp', []);
 
+tubeTravelApp.config(['$locationProvider', function($locationProvider){
+  $locationProvider.html5Mode({enable: false, rewriteLinks: false}).hashPrefix('!');
+}])
+
 tubeTravelApp.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.year = '1994';
 
-
-
+//http url
+ // '/scrape/'+$scope.year
   // $scope.changeYear = function() {
 
   // }
